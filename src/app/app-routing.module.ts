f95e746efac939315fddadb354e54a0cb7809ef6
Alfredo Razo Garduño <thexcrow@outlook.com>
2020-10-20 19:30:14 -0500

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginFirmaComponent } from '@comp/login-firma/login-firma.component';
 import { LoginComponent } from '@comp/login/login.component';
+import { MainComponent } from '@comp/main/main.component';
+import { GuardService } from '@serv/guard.service';
 
 const routes: Routes = [
  // { path: '', component: LoginComponent },
   { path: '', component: LoginFirmaComponent },
+  {
+    path: 'main', component: MainComponent, canActivate: [GuardService],
+    children: [
+       // { path: '', component: , canActivate: [GuardService] }
+    ]
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
