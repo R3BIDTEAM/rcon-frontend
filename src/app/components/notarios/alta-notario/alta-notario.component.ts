dbabdf18aa09f3c9d9cca08c9be2fefb4f4f9944
Vladimir <39971410+vladimir1189@users.noreply.github.com>
2021-04-29 18:15:57 -0500

    notario base

--- src/app/components/notarios/alta-notario/alta-notario.component.ts
+++ src/app/components/notarios/alta-notario/alta-notario.component.ts
@@ -1,4 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@env/environment';
+import { AuthService } from '@serv/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import * as moment from 'moment';
+
+export interface DatosGenerales {
+  no_notario: string;
+  estado: string;
+  nombre: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  rfc: string;
+  curp: string;
+  ine: string;
+  otro_documento: string;
+  numero_documento: string;
+  fecha_nacimiento: string;
+  fecha_defuncion: string;
+  celular: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-alta-notario',
