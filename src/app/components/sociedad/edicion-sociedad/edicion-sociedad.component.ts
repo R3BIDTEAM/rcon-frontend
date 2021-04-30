e5585c13a687eb0e73856b1ce4f40a6e3059d7de
Isaias Vidal <ividal@bidtecnologia.mx>
2021-04-29 22:44:11 -0500

    Sociedad-Perito v5.3

--- src/app/components/sociedad/edicion-sociedad/edicion-sociedad.component.ts
+++ src/app/components/sociedad/edicion-sociedad/edicion-sociedad.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatPaginator } from '@angular/material/paginator';
+import { environment } from '@env/environment'
+import { AuthService } from '@serv/auth.service';
 
 @Component({
     selector: 'app-edicion-sociedad',
@@ -6,14 +11,48 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./edicion-sociedad.component.css']
 })
 export class EdicionSociedadComponent implements OnInit {
+    endpoint = environment.endpoint + 'registro/getContribuyente';
+    displayedColumns: string[] = ['registro','nombre', 'datos', 'select'];
+    pagina = 1;
+    total = 0;
+    pagesize = 15;
+    loading = false;
+    dataSource = [];
+    httpOptions;
+    razonSocial;
+    rfc;
+    registro;
+    search;
 
-    constructor() { }
-
+    constructor(
+        private http: HttpClient,
+        private snackBar: MatSnackBar,
+        private auth: AuthService,
+    ) { }
+    
     ngOnInit(): void {
+        this.httpOptions = {
+            headers: new HttpHeaders({
+              'Content-Type': 'application/json',
+              Authorization: this.auth.getSession().token
+            })
+        };
     }
 
     clean(): void{
         // this.busqueda = false;
         // this.resetPaginator();
+    }
+
+    paginado(evt): void{
+        this.pagina = evt.pageIndex + 1;
+    }
+
+    validateSearch(){
+        this.search = (
+            this.razonSocial ||
+            this.rfc ||
+            this.registro
+        ) ? true : false;
     }
 }
