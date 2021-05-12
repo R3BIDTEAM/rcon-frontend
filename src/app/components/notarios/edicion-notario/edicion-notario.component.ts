3aa174626ec45b6932fcc47627de6fe3838acdfa
Vladimir <39971410+vladimir1189@users.noreply.github.com>
2021-05-11 19:06:48 -0500

    base edicion notario

--- src/app/components/notarios/edicion-notario/edicion-notario.component.ts
+++ src/app/components/notarios/edicion-notario/edicion-notario.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@env/environment';
+import { AuthService } from '@serv/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-edicion-notario',
