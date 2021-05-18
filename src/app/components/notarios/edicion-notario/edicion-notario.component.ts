fe8ca506bed7361a8b7bbee849b5b566b16e0d31
Vladimir <39971410+vladimir1189@users.noreply.github.com>
2021-05-18 18:18:12 -0500

    modal consulta edicion notario

--- src/app/components/notarios/edicion-notario/edicion-notario.component.ts
+++ src/app/components/notarios/edicion-notario/edicion-notario.component.ts
@@ -91,21 +91,19 @@ export class EdicionNotarioComponent implements OnInit {
 
 export class DialogBuscarNotario {
   endpointBuscarNotario = environment.endpoint + 'registro/';
-  //loadingTiposDireccion = false;
-  // loadingEstados = false;
-  // loadingMunicipios = false;
-  // loadingTiposAsentamiento = false;
-  // loadingTiposVia = false;
-  // loadingTiposLocalidad = false;
+  pagina = 1; 
+  total = 0;
+  loading = false;
+  dataSource = [];
+  displayedColumns: string[] = ['nombre','rfc','curp','seleccionar'];
   httpOptions;
-  // tiposDireccion;
-  // estados;
-  // municipios;
-  // tiposAsentamiento;
-  // tiposVia;
-  // tiposLocalidad;
-  // domicilioFormGroup: FormGroup;
   dataNotario: DataNotario = {} as DataNotario;
+  isIdentificativo: boolean;
+  canSearch = false;
+  isBusqueda;
+  queryParamFiltros;
+  endpointBusqueda;
+  @ViewChild('paginator') paginator: MatPaginator;
 
   constructor(
     private http: HttpClient,
@@ -114,5 +112,114 @@ export class DialogBuscarNotario {
     private snackBar: MatSnackBar,
     public dialog: MatDialog,
   ) { }
+
+  ngOnInit(): void {
+    this.isBusqueda = false;
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: this.auth.getSession().token
+      })
+    };
+  }
+
+  clearInputsIdentNoIdent(isIdentificativo): void {
+    this.isIdentificativo = isIdentificativo;
+
+    if(isIdentificativo){
+      this.dataNotario.apellido_paterno = null;
+      this.dataNotario.apellido_materno = null;
+      this.dataNotario.nombre = null;
+    } else {
+      this.dataNotario.rfc = null;
+      this.dataNotario.curp = null;
+      this.dataNotario.ine = null;
+      this.dataNotario.otro_documento = null;
+      this.dataNotario.numero_documento = null;
+    }
+
+  }
+
+  getData(isSearch): void {
+    this.loading = true;
+    this.isBusqueda = true;
+    this.endpointBusqueda = '';
+
+    const notario = {
+      apellidoPaterno: 'MARTINEZ',
+      filtroApellidoPaterno: '0'
+    }
+
+    if(isSearch){
+      this.queryParamFiltros = '';
+
+      if(this.isIdentificativo){
+    
+          this.endpointBusqueda = this.endpointBuscarNotario + 'getNotariosByDatosIdentificativos';
+
+          if(this.dataNotario.rfc){
+            this.queryParamFiltros = this.queryParamFiltros + 'rfc=' + this.dataNotario.rfc + '&filtroApellidoPaterno=0'; 
+          }
+
+      }else{
+
+        this.endpointBusqueda = this.endpointBuscarNotario + 'getNotariosByDatosPersonales';
+
+        if(this.dataNotario.apellido_paterno){
+          this.queryParamFiltros = this.queryParamFiltros + 'apellidoPaterno=' + this.dataNotario.apellido_paterno + '&filtroApellidoPaterno=0'; 
+        }
+        if(this.dataNotario.apellido_materno){
+          this.queryParamFiltros = this.queryParamFiltros + 'apellidoMaterno=' + this.dataNotario.apellido_materno + '&filtroApellidoMaterno=0'; 
+        }
+        if(this.dataNotario.nombre){
+          this.queryParamFiltros = this.queryParamFiltros + 'nombre=' + this.dataNotario.nombre + '&filtroNombre=0'; 
+        }
+
+      }
+
+      sessionStorage.filtrosInfEspecifica = JSON.stringify(this.dataNotario);
+      // sessionStorage.filtroSelectedInfEspecifica = this.filtroSelected;
+    } 
+    
+    this.http.post(this.endpointBusqueda + '?' + this.queryParamFiltros, '', this.httpOptions)
+      .subscribe(
+        (res: any) => {
+          (isSearch) ? this.resetPaginator() : false;
+          this.loading = false;
+          
+          console.log(res.length);
+          if(res.length == 0){
+            this.dataSource = [];
+          }else{
+            this.dataSource = res;
+            // this.dataSource = res.data;
+            this.total = res.total;
+          }
+        },
+        (error) => {
+          this.loading = false;
+          this.snackBar.open(error.error.mensaje, 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'end',
+            verticalPosition: 'top'
+          });
+        });
+  }
+
+  paginado(evt): void{
+    this.pagina = evt.pageIndex + 1;
+    this.getData(false);
+  }
+
+  clean(): void{
+    this.isBusqueda = false;
+    this.resetPaginator();
+  }
+
+  resetPaginator() {
+    this.pagina = 1;
+    this.total = 0;
+    // this.paginator.pageIndex = 0;
+  }
 
 }
