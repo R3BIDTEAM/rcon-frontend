eb108bd0c1932a3a852f793c4f072eacfb9b03df
Vladimir <39971410+vladimir1189@users.noreply.github.com>
2021-09-09 12:25:35 -0500

    sociedades ver completo sin estilos

--- src/app/components/sociedad/ver-sociedad/ver-sociedad.component.ts
+++ src/app/components/sociedad/ver-sociedad/ver-sociedad.component.ts
@@ -23,11 +23,19 @@ export class VerSociedadComponent implements OnInit {
 
     endpoint = environment.endpoint + 'registro/getSociedadValuacion';
     endpointTable = environment.endpoint + 'registro/getPeritoBySociedad';
+    endpointActualiza = environment.endpoint + 'registro/';
     displayedColumns: string[] = ['nombre','registro', 'rfc'];
     pagina = 1;
     total = 0;
     pageSize = 15;
     loading = false;
+    displayedColumnsDom: string[] = ['tipoDir', 'direccion', 'historial', 'editar'];
+    displayedColumnsInm: string[] = ['inmueble','direccion','domicilio','descripcion','sujeto'];
+    displayedColumnsRepdo: string[] = ['representacion','texto','caducidad','editar','eliminar'];
+    displayedColumnsDataRep: string[] = ['fechaCaducidad','texto','caducidad'];
+    loadingDomicilios = false;
+    loadingRepresentante = false;
+    loadingRepresentado = false;
     dataSocedadResultado;
     dataSource;
     dataPaginate;
@@ -37,6 +45,35 @@ export class VerSociedadComponent implements OnInit {
     idSociedad;
     datosSociedad: DatosSociedad = {} as DatosSociedad;
     @ViewChild('paginator') paginator: MatPaginator;
+
+    /*Paginado*/
+    dataSource1 = [];
+    total1 = 0;
+    pagina1= 1;
+    dataPaginate1;
+    dataSource2 = [];
+    total2 = 0;
+    pagina2= 1;
+    dataPaginate2;
+    dataSource3 = [];
+    total3 = 0;
+    pagina3= 1;
+    dataPaginate3;
+    dataSource4 = [];
+    total4 = 0;
+    pagina4= 1;
+    dataPaginate4;
+    dataSource5 = [];
+    total5 = 0;
+    pagina5= 1;
+    dataPaginate5;
+    total6 = 0;
+    pagina6= 1;
+    dataPaginate6;
+    total7 = 0;
+    pagina7= 1;
+    dataPaginate7;
+   /*Paginado*/
 
     constructor(
         private http: HttpClient,
@@ -55,6 +92,9 @@ export class VerSociedadComponent implements OnInit {
         this.idSociedad = this.route.snapshot.paramMap.get('idsociedad');
         console.log(this.idSociedad);
         this.getSociedadDatos();
+        this.getDomicilioSociedad();
+        this.getRepresentacion();
+        this.getRepresentado();
     }
 
     getSociedadDatos(){
@@ -113,9 +153,127 @@ export class VerSociedadComponent implements OnInit {
         this.datosSociedad.fecha_baja = (this.dataSocedadResultado.FECHABAJA) ? new Date(this.dataSocedadResultado.FECHABAJA) : null;
     }
 
+    /**
+     * Obtiene los domicilios registrados de la sociedad domicilios particulares y para recibir notificaciones.
+     */
+    getDomicilioSociedad(){
+    this.loadingDomicilios = true;
+    let metodo = 'getDireccionesContribuyente';
+    this.http.get(this.endpointActualiza + metodo + '?idPersona='+ this.idSociedad, this.httpOptions)
+        .subscribe(
+            (res: any) => {
+                this.loadingDomicilios = false;
+
+                this.dataSource1 = res.filter(element => element.CODTIPOSDIRECCION !== "N");
+                this.dataSource2 = res.filter(element => element.CODTIPOSDIRECCION === "N");
+                this.total1 = this.dataSource1.length;
+                this.total2 = this.dataSource2.length;
+                this.dataPaginate1 = this.paginate(this.dataSource1, 15, this.pagina1);
+                this.dataPaginate2 = this.paginate(this.dataSource2, 15, this.pagina2);
+            },
+            (error) => {
+                this.loadingDomicilios = false;
+                this.snackBar.open(error.error.mensaje, 'Cerrar', {
+                    duration: 10000,
+                    horizontalPosition: 'end',
+                    verticalPosition: 'top'
+                });
+            }
+        );
+    }
+
+    /**
+     * Obtiene las representaciónes de la sociedad.
+     */
+     getRepresentacion(){
+        this.loadingRepresentante = true;
+        let queryRep = 'rep=Representantes&idPersona=' + this.idSociedad;
+        this.http.get(this.endpointActualiza + 'getRepresentacionContribuyente?' + queryRep, this.httpOptions)
+            .subscribe(
+                (res: any) => {
+                    this.loadingRepresentante = false;
+                    this.dataSource4 = res;
+                    this.total4 = this.dataSource4.length;
+                    this.dataPaginate4 = this.paginate(this.dataSource4, 15, this.pagina4);
+                },
+                (error) => {
+                    this.loadingRepresentante = false;
+                    this.snackBar.open(error.error.mensaje, 'Cerrar', {
+                        duration: 10000,
+                        horizontalPosition: 'end',
+                        verticalPosition: 'top'
+                    });
+                }
+            );
+    }
+
+    /**
+     * Obtienen los representados de la sociedad.
+     */
+     getRepresentado(){
+        this.loadingRepresentado = true;
+        let queryRepdo = 'rep=Representado&idPersona=' + this.idSociedad;
+        console.log(this.endpointActualiza + 'getRepresentacionContribuyente?' + queryRepdo);
+        this.http.get(this.endpointActualiza + 'getRepresentacionContribuyente?' + queryRepdo, this.httpOptions)
+            .subscribe(
+                (res: any) => {
+                    this.loadingRepresentado = false;
+                    this.dataSource5 = res;
+                    console.log("ACA ENTRO EL REPRESENTADO");
+                    console.log(res);
+                    this.total5 = this.dataSource5.length;
+                    this.dataPaginate5 = this.paginate(this.dataSource5, 15, this.pagina5);
+                },
+                (error) => {
+                    this.loadingRepresentado = false;
+                    this.snackBar.open(error.error.mensaje, 'Cerrar', {
+                        duration: 10000,
+                        horizontalPosition: 'end',
+                        verticalPosition: 'top'
+                    });
+                }
+            );
+    }
+
     paginado(evt): void{
         this.pagina = evt.pageIndex + 1;
         this.dataSource = this.paginate(this.dataSource, this.pageSize, this.pagina);
+    }
+
+    /**
+     * Método del paginado que nos dira la posición del paginado y los datos a mostrar
+     * @param evt Nos da la referencia de la pagina en la que se encuentra
+     */
+     paginado1(evt): void{
+        this.pagina1 = evt.pageIndex + 1;
+        this.dataSource1 = this.paginate(this.dataSource1, 15, this.pagina1);
+    }
+
+    /**
+     * Método del paginado que nos dira la posición del paginado y los datos a mostrar
+     * @param evt Nos da la referencia de la pagina en la que se encuentra
+     */
+    paginado2(evt): void{
+        this.pagina1 = evt.pageIndex + 1;
+        this.dataSource1 = this.paginate(this.dataSource1, 15, this.pagina1);
+    }
+
+    /**
+     * Método del paginado que nos dira la posición del paginado y los datos a mostrar
+     * @param evt Nos da la referencia de la pagina en la que se encuentra
+     */
+     paginado4(evt): void{
+        this.pagina4 = evt.pageIndex + 1;
+        this.dataSource4 = this.paginate(this.dataSource4, 15, this.pagina4);
+    }
+
+    /**
+     * Método del paginado que nos dira la posición del paginado y los datos a mostrar
+     * @param evt Nos da la referencia de la pagina en la que se encuentra
+     */
+     paginado5(evt): void{
+        this.pagina5 = evt.pageIndex + 1;
+        this.dataSource5 = this.paginate(this.dataSource5, 15, this.pagina5);
     }
 
     paginate(array, page_size, page_number) {
