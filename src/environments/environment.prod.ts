424e1d47ad23a7a87fa6813f0557bb57c5916ea3
iovidal33 <iovidal33@gmail.com>
2022-05-05 18:06:00 -0500

    SOCIEDAD FECHA ALTA DISABLED - ENVIROMENT PROD V1.97

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
-  appName: 'base',
-  endpoint: '',
-  ssoEndpoint: '',
-  ssoToken: '',
+  closeSession: true,
+  baseHref: '/',
+  appName: 'RCON',
+  endpoint: 'https://ovica-servicios.finanzas.cdmx.gob.mx/rcon-backend/public/api/v1/',
+  ssoEndpoint: 'https://ovica-servicios.finanzas.cdmx.gob.mx/sso-backend/public/api/v1/',
+  ssoRemember: 'https://ovica-servicios.finanzas.cdmx.gob.mx/sso/recordar-password',
+  rconEndpoint: 'https://ovica-servicios.finanzas.cdmx.gob.mx/rcon-backend/public/api/v1/',
+  ssoToken: 'eyJpdiI6ImhpdHI3YU53OVlGd3hXSDM1RWRZOEE9PSIsInZhbHVlIjoiUklZSkJQMHJMcUNEWXVhVEpET085eGd2bHBmZzN5K0hoandTSGluVUp5VW1NZmtZOW9LZkxiRnVkbjk5RE5sSzY5elJqVHJnbmJaYVh6d1ExaFIvcEE3K3pMdTBMQWwvL09UeEVLNHV5ZmhWc1NBbysza1U3cnFySnVTNVMzT0QzeFVlZFhtWVo2V1UvYlE0MitqSWc4WmdZVG55U0JKZ2ExbFIvcGg4M3NzamtnMXZXNFQxR0FVZWNVWFhYL0VLMXJkL0ZGUGt5MFRMK3NQTUYwdWp6dFVKbXh6Mmh0V1dKbGg4ay9Rc1pOZmJVSjIxYWM3S1lmNTVkcVJST2tpOHRzaWxpbVc5QjZpUkhmUWJydFFMWEM5U0xFV082NTJWNGwrcE16WjUvWmtuYlo5SVBaQ1hCTjg3QW12Nkh0V2NrelBibmRSV2Y2NCtUTzdyOTZoK3NRdDBQM0lIdDdndldMOEZxbmlxUWdsVnluVlNvL2xwUHRYMHhZb3lCY05lY1loZDFWZXczVFJTNk1RMzZGT0FZUDJ5Q1BwOGkwTmJXemRLaktLdGc1a1hrOW4rUlBEejBmdk5hNVBGeTgwWVlSMFZPL2hCRGloaE1IWG41bVpnRGc9PSIsIm1hYyI6ImM0MDM0NWVmZmQ4ODZmNzhkZTExYjNiZGExZGFkNGViMzUzYzlkNDllOTU1NTNiZWYwOWYyYzhhMTU1NDczMGMifQ==',
 };
