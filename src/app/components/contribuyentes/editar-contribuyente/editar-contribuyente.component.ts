adf1d3c6a647f6e553781207c03094795c49e250
Vladimir <39971410+vladimir1189@users.noreply.github.com>
2022-07-06 20:10:00 -0500

    cambios editar contribuyente 2.0

--- src/app/components/contribuyentes/editar-contribuyente/editar-contribuyente.component.ts
+++ src/app/components/contribuyentes/editar-contribuyente/editar-contribuyente.component.ts
@@ -742,7 +742,26 @@ export class EditarContribuyenteComponent implements OnInit {
                     this.datoDelContribuyente();
                     if(res.infoComprobante){
                         console.log("AQUI INFO COMPROBANTE");
-                        this.actualizadoCuenta = true;
+                        this.dataActualizacion.after_CP = res.after_CP;
+                        this.dataActualizacion.after_Col = res.after_Col;
+                        this.dataActualizacion.after_Direccion = res.after_Direccion;
+                        this.dataActualizacion.after_Nombre = res.after_Nombre;
+                        this.dataActualizacion.after_RFC = res.after_RFC;
+                        this.dataActualizacion.after_cuenta = res.after_cuenta;
+                        this.dataActualizacion.area = res.area;
+                        this.dataActualizacion.at = res.at;
+                        this.dataActualizacion.before_cuenta = res.before_cuenta;
+                        this.dataActualizacion.before_CP = res.before_CP;
+                        this.dataActualizacion.before_Col = res.before_Col;
+                        this.dataActualizacion.before_Direccion = res.before_Direccion;
+                        this.dataActualizacion.before_Nombre = res.before_Nombre;
+                        this.dataActualizacion.before_RFC = res.before_RFC;
+                        this.dataActualizacion.cuentaP = res.cuentaP;
+                        this.dataActualizacion.fechaConsulta = res.fechaConsulta;
+                        this.dataActualizacion.folio = res.folio;
+                        this.dataActualizacion.idpersona = res.idpersona;
+                        this.dataActualizacion.usuario = res.usuario;
+                        this.actualizado = true;
                     }
                 },
                 (error) => {
