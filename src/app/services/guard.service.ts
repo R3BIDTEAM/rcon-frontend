92df59278ffa8227136d988932bdcd68e7aaf7a9
iovidal33 <iovidal33@gmail.com>
2022-07-18 13:14:05 -0500

    GUARD SERVICE TS V1.114

--- src/app/services/guard.service.ts
+++ src/app/services/guard.service.ts
@@ -11,35 +11,110 @@ export class GuardService implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
         this.rol = this.auth.getSession().userData.rol_nombre;
-        console.log(this.rol);
+        console.log(route);
         switch (this.rol) {
-            case "RCONSAT":
-                if(route.url[0].path === 'main' || route.url[0].path === 'alta-contribuyente' ||  route.url[0].path === 'edicion-contribuyente' || route.url[0].path === 'editar-contribuyente'){
+            case "SUPERVISOR RCON":
+                if(route.url[0].path === 'main' 
+                    || route.url[0].path === 'reporte'
+                    || route.url[0].path === 'consulta-contribuyente' 
+                    || route.url[0].path === 'consulta-notario' 
+                    || route.url[0].path === 'consulta-peritos'
+                    || route.url[0].path === 'consulta-sociedad'
+                    || route.url[0].path === 'ver-peritos' 
+                    || route.url[0].path === 'ver-sociedad' 
+                    || route.url[0].path === 'ver-notario'
+                    || route.url[0].path === 'ver-contribuyente'
+                ){
                     return this.auth.isAuthenticated();
                 }else{
                     this.router.navigate(['/main']);
                 }
             break;
-            case 'RCONCSAT':
-                if(route.url[0].path === 'main' || route.url[0].path == 'consulta-contribuyente' || route.url[0].path == 'ver-contribuyente'){
+            case 'CONSULTOR RCON':
+                if(route.url[0].path === 'main' 
+                    || route.url[0].path === 'consulta-notario' 
+                    || route.url[0].path === 'consulta-peritos'
+                    || route.url[0].path === 'consulta-sociedad'
+                    || route.url[0].path === 'ver-peritos' 
+                    || route.url[0].path === 'ver-sociedad' 
+                    || route.url[0].path === 'ver-notario'
+                ){
                     return this.auth.isAuthenticated();
                 }else{
                     this.router.navigate(['/main']);
                 }
             break;
-            case "RCONPERITOS":
-                if(route.url[0].path == "alta-notario" ||  route.url[0].path == "edicion-notario" ||  route.url[0].path == "main" || route.url[0].path == "editar-notario"
-                    || route.url[0].path == "alta-peritos" || route.url[0].path == "edicion-peritos" || route.url[0].path == "editar-peritos"
-                    || route.url[0].path == "alta-sociedad" || route.url[0].path == "edicion-sociedad" || route.url[0].path == "editar-sociedad"){
+            case "EDITOR RCON":
+                if(route.url[0].path === "main" 
+                    || route.url[0].path == "alta-notario" 
+                    || route.url[0].path == "edicion-notario" 
+                    || route.url[0].path == "editar-notario"
+                    || route.url[0].path == "alta-peritos" 
+                    || route.url[0].path == "edicion-peritos"
+                    || route.url[0].path == "editar-peritos"
+                    || route.url[0].path == "alta-sociedad"
+                    || route.url[0].path == "edicion-sociedad"
+                    || route.url[0].path == "editar-sociedad"
+                ){
                     return this.auth.isAuthenticated();
                 }else{
                     this.router.navigate(['/main']);
                 }
             break;
-            case "RCONCPERITOS":
-                if(route.url[0].path == "consulta-notario" ||  route.url[0].path == "main" || route.url[0].path == "consulta-peritos"
-                    || route.url[0].path == "consulta-sociedad" || route.url[0].path == "ver-peritos"
-                    || route.url[0].path == "ver-sociedad" || route.url[0].path == "ver-notario"){
+            case "AUDITORIA RCON":
+                if(route.url[0].path === "main"
+                    || route.url[0].path === 'consulta-contribuyente' 
+                    || route.url[0].path === "consulta-notario" 
+                    || route.url[0].path === "consulta-peritos"
+                    || route.url[0].path === "consulta-sociedad" 
+                    || route.url[0].path === 'ver-contribuyente'
+                    || route.url[0].path === "ver-peritos"
+                    || route.url[0].path === "ver-sociedad" 
+                    || route.url[0].path === "ver-notario"
+                ){
+                    return this.auth.isAuthenticated();
+                }else{
+                    this.router.navigate(['/main']);
+                }
+            break;
+            case 'CONSULTOR CONT RCON':
+                if(route.url[0].path === 'main' 
+                    || route.url[0].path === 'consulta-contribuyente' 
+                    || route.url[0].path === 'ver-contribuyente'
+                ){
+                    return this.auth.isAuthenticated();
+                }else{
+                    this.router.navigate(['/main']);
+                }
+            break;
+            case 'EDITOR CONT RCON':
+                if(route.url[0].path === 'main' 
+                    || route.url[0].path === 'alta-contribuyente' 
+                    || route.url[0].path === 'edicion-contribuyente'
+                    || route.url[0].path === 'editar-contribuyente'
+                    || route.url[0].path === 'editart-contribuyente'
+                ){
+                    return this.auth.isAuthenticated();
+                }else{
+                    this.router.navigate(['/main']);
+                }
+            break;
+            case 'Administrador':
+                if(route.url[0].path === 'main' 
+                    || route.url[0].path === 'alta-contribuyente' 
+                    || route.url[0].path === 'edicion-contribuyente'
+                    || route.url[0].path === 'editar-contribuyente'
+                    || route.url[0].path === 'editart-contribuyente'
+                    || route.url[0].path === 'reporte'
+                    || route.url[0].path === 'consulta-contribuyente' 
+                    || route.url[0].path === 'consulta-notario' 
+                    || route.url[0].path === 'consulta-peritos'
+                    || route.url[0].path === 'consulta-sociedad'
+                    || route.url[0].path === 'ver-peritos' 
+                    || route.url[0].path === 'ver-sociedad' 
+                    || route.url[0].path === 'ver-notario'
+                    || route.url[0].path === 'ver-contribuyente'
+                ){
                     return this.auth.isAuthenticated();
                 }else{
                     this.router.navigate(['/main']);
